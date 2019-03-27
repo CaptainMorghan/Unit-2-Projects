d91848424954717310e89cf9bd8f6deb36c225d1
CaptainMorghan <47458916+CaptainMorghan@users.noreply.github.com>
2019-03-27 13:30:02 -0400

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-//Morghan Kiverago 25/03/2019
-// I speak TXTMSG
+//Morghan Kiverago 3/27/2019
+// Tshit Design
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,7 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Unit_2_project
+namespace Unit_2_project_4
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -25,120 +25,52 @@ namespace Unit_2_project
         public MainWindow()
         {
             InitializeComponent();
+            for (int x = 0; x < 8; x++)
+            {
+                for (int y = 0; y < 8; y++)
+                {
+                    Ellipse e = new Ellipse();
+                    e.Height = 100;
+                    e.Width = 100;
+
+                    if (x % 2 == 1 && y % 2 == 0 || x % 2 == 0 && y % 2 == 1)
+                    {
+                        e.Fill = Brushes.Red;
+                    }
+                    else
+                    {
+                        e.Fill = Brushes.Blue;
+                    }
+                    canvas.Children.Add(e);
+                    Canvas.SetTop(e, x * 100);
+                    Canvas.SetLeft(e, y * 100);
+                }
+            }
+            for (int x = 0; x < 8; x++)
+            {
+                for (int y = 0; y < 8; y++)
+                {
+                    Rectangle r = new Rectangle();
+                    r.Height = 50;
+                    r.Width = 50;
+
+                    if (x % 2 == 1 && y % 2 == 0 || x % 2 == 0 && y % 2 == 1)
+                    {
+                        r.Fill = Brushes.Pink;
+                    }
+                    else
+                    {
+                        r.Fill = Brushes.Purple;
+                    }
+                    canvas.Children.Add(r);
+                    Canvas.SetTop(r, x * 100);
+                    Canvas.SetLeft(r, y * 100);
+
+                }
+            }
+
+
         }
-
-        private void btnExplain_Click(object sender, RoutedEventArgs e)
-        {
-
-
-
-            if (txtInput.Text == "CU") //If statement to determine if true. if it is it opens a message box to display the translation
-            {
-                //CHANGE THIS
-                MessageBox.Show("See you");
-            }//Copy paste for each code/translation
-            else if (txtInput.Text == ":-)")
-            {
-                MessageBox.Show("I'm Happy");
-            }
-            else if (txtInput.Text == ":-(")
-            {
-                MessageBox.Show("I'm sad");
-            }
-            else if (txtInput.Text == ";-)")
-            {
-                MessageBox.Show("wink");
-            }
-            else if (txtInput.Text == ":-P")
-            {
-                MessageBox.Show("stick out my toung");
-            }
-            else if (txtInput.Text == "(~.~)")
-            {
-                MessageBox.Show("sleepy");
-            }
-            else if (txtInput.Text == "TA")
-            {
-                MessageBox.Show("Totally Awsome");
-            }
-            else if (txtInput.Text == "CCC")
-            {
-                MessageBox.Show("Canadian Computing Competition");
-            }
-            else if (txtInput.Text == "CUZ")
-            {
-                MessageBox.Show("Because");
-            }
-            else if (txtInput.Text == "TY")
-            {
-                MessageBox.Show("Thank you");
-            }
-            else if (txtInput.Text == "YW")
-            {
-                MessageBox.Show("You're Welcome");
-            }
-            else if (txtInput.Text == "TTYL")
-            {
-                MessageBox.Show("Talk to you later");
-            }
-            if (txtInput.Text == "See you")  // now to make it work in reverse
-            {
-                
-                MessageBox.Show("CU");
-            }
-            else if (txtInput.Text == "I'm happy")
-            {
-                MessageBox.Show(":-)");
-            }
-            else if (txtInput.Text == "I'm sad")
-            {
-                MessageBox.Show(":-(");
-            }
-            else if (txtInput.Text == "wink")
-            {
-                MessageBox.Show(";-)");
-            }
-            else if (txtInput.Text == "stick out my toung")
-            {
-                MessageBox.Show(":-P");
-            }
-            else if (txtInput.Text == "sleepy")
-            {
-                MessageBox.Show("(~.~)");
-            }
-            else if (txtInput.Text == "totally awsome")
-            {
-                MessageBox.Show("TA");
-            }
-            else if (txtInput.Text == "Canadian Computing Competition")
-            {
-                MessageBox.Show("CCC");
-            }
-            else if (txtInput.Text == "because")
-            {
-                MessageBox.Show("CUZ");
-            }
-            else if (txtInput.Text == "Thank you")
-            {
-                MessageBox.Show("TY");
-            }
-            else if (txtInput.Text == "You're Welcome")
-            {
-                MessageBox.Show("YW");
-            }
-            else if (txtInput.Text == "Talk to you later")
-            {
-                MessageBox.Show("TTYL");
-            }
-
-
-
-
-
-
-
-
         }
     }
-}
 
